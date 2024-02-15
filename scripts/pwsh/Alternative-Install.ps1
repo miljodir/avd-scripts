078316964470dbfadd35242974fcaa0f4a3b3b9f
audunsolemdal <audun@solom.no>
2024-02-15 08:26:19 +0100

    Update alt install

--- scripts/pwsh/Alternative-Install.ps1
+++ scripts/pwsh/Alternative-Install.ps1
@@ -18,76 +18,13 @@ if ((Test-Path c:\temp) -eq $false) {
         "creating temp directory"
 }
 
-# credit to: https://github.com/ChrisTitusTech/winutil/blob/a1c7501b98d56c5b554f9cfe70cb5a0767385796/functions/private/Install-WinUtilWinget.ps1#L1
-
+# Pre-installed winget on win 11 is buggy and should be removed and reinstalled with latest version
+# https://github.com/microsoft/winget-cli/issues/3832#issuecomment-1872387214
 function Install-WinUtilWinget {
     
-    <#
-    
-        .DESCRIPTION
-        Function is meant to ensure winget is installed 
-    
-    #>
-    Try {
-        Write-Host "Checking if Winget is Installed..."
-        if (Test-WinUtilPackageManager -winget) {
-            #Checks if winget executable exists and if the Windows Version is 1809 or higher
-            Write-Host "Winget Already Installed"
-            return
-        }
-
-        #Gets the computer's information
-        if ($null -eq $sync.ComputerInfo) {
-            $ComputerInfo = Get-ComputerInfo -ErrorAction Stop
-        }
-        Else {
-            $ComputerInfo = $sync.ComputerInfo
-        }
-
-        if (($ComputerInfo.WindowsVersion) -lt "1809") {
-            #Checks if Windows Version is too old for winget
-            Write-Host "Winget is not supported on this version of Windows (Pre-1809)"
-            return
-        }
-
-        #Gets the Windows Edition
-        $OSName = if ($ComputerInfo.OSName) {
-            $ComputerInfo.OSName
-        }
-        else {
-            $ComputerInfo.WindowsProductName
-        }
-
-        if (((($OSName.IndexOf("LTSC")) -ne -1) -or ($OSName.IndexOf("Server") -ne -1)) -and (($ComputerInfo.WindowsVersion) -ge "1809")) {
-
-            Write-Host "Running Alternative Installer for LTSC/Server Editions"
-
-            # Switching to winget-install from PSGallery from asheroto
-            # Source: https://github.com/asheroto/winget-installer
-
-            Start-Process powershell.exe -Verb RunAs -ArgumentList "-command irm https://raw.githubusercontent.com/ChrisTitusTech/winutil/$BranchToUse/winget.ps1 | iex | Out-Host" -WindowStyle Normal -ErrorAction Stop
-
-            if (!(Test-WinUtilPackageManager -winget)) {
-                break
-            }
-        }
-
-        else {
-            #Installing Winget from the Microsoft Store
-            Write-Host "Winget not found, installing it now."
-            Start-Process "ms-appinstaller:?source=https://aka.ms/getwinget"
-            $nid = (Get-Process AppInstaller).Id
-            Wait-Process -Id $nid
-
-            if (!(Test-WinUtilPackageManager -winget)) {
-                break
-            }
-        }
-        Write-Host "Winget Installed"
-    }
-    Catch {
-        Write-Error "Failed to install winget"
-    }
+    Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile winget.msixbundle
+    Add-AppxPackage -ForceApplicationShutdown winget.msixbundle
+    del winget.msixbundle
 }
 
 Install-WinUtilWinget
